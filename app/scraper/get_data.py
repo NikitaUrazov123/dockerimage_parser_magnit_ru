8fbcd56b4686da55f19cc0ac5c948c0f78c68155
3e13d2bb-c2a8-43bc-9e2e-6e2a83ad8028 <n.urazov@belfood.by>
2025-07-09 11:54:49 +0300

    get_product_parameters

--- app/scraper/get_data.py
+++ app/scraper/get_data.py
@@ -8,40 +8,50 @@ from sqlalchemy.dialects.postgresql import insert
 from utils import generate_id
 from db_config import SQLALCHEMY_DATABASE_URL
 from requests.exceptions import RequestException
+import json
 
 # -------------------------------------------------------------------------------------------------------
-def get_product_name(text: str) -> str | None:
-    soup = BeautifulSoup(text, 'html.parser')
+def get_product_name(html: str) -> str | None:
+    soup = BeautifulSoup(html, 'html.parser')
     el = soup.find('span', attrs={'data-test-id': 'v-product-details-name'})
     return el.get_text(strip=True) if el else None
 
-def get_current_price(text: str) -> float | None:
-    soup = BeautifulSoup(text, 'html.parser')
+def get_current_price(html: str) -> float | None:
+    soup = BeautifulSoup(html, 'html.parser')
     el = soup.find('span', attrs={'data-test-id': 'v-product-detail-price-current'})
     if el:
-        raw_string = el.get_text(strip=True)
-        clean = raw_string.replace('₽', '').strip()
-        try:
-            return float(clean)
-        except ValueError:
-            return None
+        clean = el.get_text(strip=True).replace('₽', '').strip()
+        try: return float(clean)
+        except ValueError: return None
     return None
 
-def get_old_price(text: str) -> float | None:
-    soup = BeautifulSoup(text, 'html.parser')
+def get_old_price(html: str) -> float | None:
+    soup = BeautifulSoup(html, 'html.parser')
     el = soup.find('span', attrs={'data-test-id': 'v-product-detail-price-old'})
     if el:
-        raw_string = el.get_text(strip=True)
-        clean = raw_string.replace('₽', '').strip()
-        try:
-            return float(clean)
-        except ValueError:
-            return None
+        clean = el.get_text(strip=True).replace('₽', '').strip()
+        try: return float(clean)
+        except ValueError: return None
     return None
+
+def get_product_parameters(html: str) -> dict:
+    soup = BeautifulSoup(html, 'html.parser')
+    result = {}
+    for item in soup.select(".product-details-parameters-list__item"):
+        name_el = item.select_one("[data-test-id='v-product-details-parameters-list-item-name']")
+        value_el = item.select_one("[data-test-id='v-product-details-parameters-list-item-value']")
+        if name_el and value_el:
+            name = name_el.get_text(strip=True)
+            value = value_el.get_text(strip=True)
+            result[name] = value
+    return result
+
 # -------------------------------------------------------------------------------------------------------
 HEADERS = {
     'Accept': '*/*',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    'user-agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/95.0.4638.69 Safari/537.36')
 }
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -49,8 +59,6 @@ metadata = MetaData(schema="online_market_monitoring")
 magnit_data_table = Table("magnit_data", metadata, autoload_with=engine)
 
 valid_columns = {col.name for col in magnit_data_table.columns}
-
-today_str = date.today().isoformat()
 
 with engine.begin() as conn:
     rows = conn.execute(
@@ -68,18 +76,20 @@ for link_id, link in rows:
         time.sleep(2)
         continue
 
+    today_str = date.today().isoformat()
+    
     record = {
-        "record_id":    generate_id(today_str, link),
-        "link":         link,
+        "record_id": generate_id(today_str, link),
+        "link": link,
         "product_name": get_product_name(html),
-        "current_price":get_current_price(html),
-        "old_price":    get_old_price(html),
+        "current_price": get_current_price(html),
+        "old_price": get_old_price(html),
+        "product_parameters": get_product_parameters(html),
     }
 
     filtered_record = {k: v for k, v in record.items() if k in valid_columns}
 
     with engine.begin() as conn:
-
         conn.execute(
             text("UPDATE online_market_monitoring.magnit_links SET is_scraped = true WHERE id = :id"),
             {"id": link_id}
@@ -89,7 +99,7 @@ for link_id, link in rows:
         stmt = stmt.on_conflict_do_nothing(index_elements=["record_id"])
         conn.execute(stmt)
 
-    logger.logging.info(f"Запись {filtered_record.get('record_id')}")
+    logger.logging.info(f"Записан {filtered_record.get('record_id')}")
     time.sleep(3)
 
 logger.logging.info("=== Done ===")
